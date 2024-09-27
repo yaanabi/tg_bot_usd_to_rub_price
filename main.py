5c9c514c84fef63cbd056d4b75b08f147adb20c8
Nabi <nabi.usonov@gmail.com>
2024-09-27 23:32:24 +0500

    Added currency exchange functionality

--- main.py
+++ main.py
@@ -31,25 +31,38 @@ async def get_usd_to_rub():
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     "Send a message when /start is issued"
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
+    context.user_data['awaiting_name'] = True
+    await update.message.reply_text(
+        "Добрый день, как вас зовут?",
         reply_markup=ForceReply(selective=True),
     )
 
 
-async def help_command(update: Update,
-                       context: ContextTypes.DEFAULT_TYPE) -> None:
-    "Send a message when the command /help is issued"
-    await update.message.reply_text("Help!")
+async def reset_name(update: Update,
+                     context: ContextTypes.DEFAULT_TYPE) -> None:
+    context.user_data['awaiting_name'] = False
+    context.user_data['name'] = None
+    await update.message.reply_text('Имя удаленно. Введите /start')
 
 
 async def usd_to_rub_command(update: Update,
                              context: ContextTypes.DEFAULT_TYPE) -> None:
-    result = await get_usd_to_rub()
-    result = round((result), 2)
-    msg = f"1 USD = {result} RUB"
-    await update.message.reply_text(msg)
+    if context.user_data.get('awaiting_name'):
+        context.user_data['awaiting_name'] = False
+        name = update.message.text
+        context.user_data['name'] = name
+        result = await get_usd_to_rub()
+        result = int(result)
+        msg = f"Рад знакомству, {name}! Курс доллара сегодня {result}р."
+        await update.message.reply_text(msg)
+    elif context.user_data.get('name'):
+        name = context.user_data.get('name')
+        result = await get_usd_to_rub()
+        result = int(result)
+        msg = f"Рад знакомству, {name}! Курс доллара сегодня {result}р."
+        await update.message.reply_text(msg)
+    else:
+        await update.message.reply_text('Введите /start')
 
 
 def main() -> None:
@@ -57,7 +70,7 @@ def main() -> None:
     app = Application.builder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("help", help_command))
+    app.add_handler(CommandHandler("reset_name", reset_name))
     app.add_handler(
         MessageHandler(filters.TEXT & ~filters.COMMAND, usd_to_rub_command))
 
@@ -66,20 +79,3 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
-
-# async def f():
-#     print('Start')
-#     await asyncio.sleep(5)
-#     print("sleep 5 ends")
-
-# async def main():
-
-#     task1 = asyncio.create_task(get_usd_to_rub())
-#     task2 = asyncio.create_task(f())
-
-#     result = asyncio.gather(task1, task2)
-#     usd_to_rub = await result
-
-#     print(usd_to_rub)
-
-# asyncio.run(main())
